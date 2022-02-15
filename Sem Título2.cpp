ec6ab979a613017424706f06ca202d05f590c5f5
DaniloArujo <88139297+DaniloArujo@users.noreply.github.com>
2022-02-15 10:14:14 -0300

    Add files via upload

---
+++ Sem Título2.cpp
@@ -0,0 +1,16 @@
+/*O seu primeiro progama em qualquer linguagem de programação
+normalmente é o "hello world!". 
+Neste primeiro problema tudo o que você precisa 
+fazer é imprimir esta mensagemna tela.*/
+//comentando um bloco de linhas
+/*comentando um bloco de linhas*/
+
+#include<stidio.h>
+
+int main(){ //inicio do algoritimo
+	
+	printf("hello world!");
+
+
+
+} //fim do algoritimo
---
+++ aula1.c
@@ -0,0 +1,15 @@
+#include <studio.h> //biblioteca
+
+
+int main(){//inicio do algoritimo
+	int idade; //inteiro idade;
+	printf("Digite a sua idade"); //escreva ("Digite a usa idade")
+	scanf("%d", &idade); //leia(idade);
+	printf("a idade digitada foi %d",idade);
+						
+			
+
+
+} //final do algoritimo
+
+
