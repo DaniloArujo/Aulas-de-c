cf2034ea6fa6251718b2df78afefbdd7b392bd5b
DaniloArujo <88139297+DaniloArujo@users.noreply.github.com>
2022-02-15 10:48:25 -0300

    Add files via upload

---
+++ Sem Título1.cpp
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+int main(){
+	double a, b, , x;
+	
+    
+	printf("digite o primeiro número: ");
+    scanf("%lf",&a);
+    
+    printf("Digite o segundo número: ");
+    scanf("%lf",&b);
+   
+    x = (a * 3.5 + b * 7.5)/11; 
+	
+	printf("X = %lf", x);
+    
+}
