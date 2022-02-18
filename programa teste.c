0065ec97a1a5ed84a12fae887449b984f956bef7
Danilo Araujo Mota <88139297+DaniloArujo@users.noreply.github.com>
2022-02-18 11:21:01 -0300

    Add files via upload

---
+++ programa teste.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int idade;
+	
+	printf("Digite uma idade: ");
+	scanf("%d",&idade);
+	
+	
+    if(idade >0 && idade<=10)
+		printf("Criança\n");
+		else if (idade>11 && idade<18)
+			printf("adolescente\n");
+				else if (idade>19 && idade<100)
+				printf("idoso\n");
+					else if (idade>101 && idade<150)
+					    printf("zumbi\n");
+				    	else printf("idade inválida");
+    
+}
