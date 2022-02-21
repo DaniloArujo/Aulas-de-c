8890a230c84c93cdd16b21f9c2f8e9fe5d93dc8a
Danilo Araujo Mota <88139297+DaniloArujo@users.noreply.github.com>
2022-02-21 09:44:03 -0300

    Add files via upload

---
+++ Exercício bhascara.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	double a, b, c, delta, bhascara1, bhascara2;
+	
+	printf("Escreva o valor de A: ");
+	scanf("%lf",&a);
+	printf("Escreva o valor de B: ");
+	scanf("%lf",&b);
+	printf("Escreva o valor de C: ");
+	scanf("%lf",&c);
+	
+	delta = pow(b,2)-4*a*c;
+	bhascara1 = ((-1)*b + sqrt(delta))/(2*a);
+	bhascara2 = ((-1)*b - sqrt(delta))/(2*a);
+	
+	if(delta < 0 && a == 0)
+		printf("impossível calcular");
+		
+		else printf("%lf  e %lf sao as raizes da equacao.",&bhascara1,&bhascara2);
+	
+	
+}
